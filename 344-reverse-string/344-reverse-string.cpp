244b35d10c6c9157819338207985303df1a6405d
Bidipta Saikia <76623158+BIDIPTA05@users.noreply.github.com>
2022-09-07 02:08:13 +0530

    Time: 31 ms (65.91%), Space: 23.2 MB (40.91%) - LeetHub

---
+++ 344-reverse-string/344-reverse-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+    int n = s.size();
+    int low =0;
+    int high = n-1;
+    while(low<high){
+        swap(s[low],s[high]);
+        low++;
+        high--;
+    }
+        
+    }
+};
