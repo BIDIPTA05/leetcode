a33e43ea20499739d2af60e2efdedb0ceaed9735
Bidipta Saikia <76623158+BIDIPTA05@users.noreply.github.com>
2022-09-12 01:43:55 +0530

    Time: 19 ms (21.40%), Space: 13.8 MB (17.94%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,26 +1,89 @@
+//OPTIMAL
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
         int n = nums.size();
         vector <int> ans;
-        int start = -1;
-        int end = -1;
-        
-        for(int i =0; i< n ;i++){
-            if(nums[i]==target){
-                start =i;
-                break;
-            }    
+        int low=0;
+        int high=n-1;
+        int fr =-1;
+     
+        if(n==0){
+            return {-1,-1};
         }
         
-          for(int i =n-1; i>= 0 ;i--){
-            if(nums[i]==target){
-                end =i;
-                break;
-            }    
-        }
-        ans.push_back(start);
-        ans.push_back(end);
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(nums[mid]==target){
+             fr = mid;
+            high = mid -1;
+            }
+            else if(nums[mid]<target){
+                low = mid +1;
+            }
+            else{
+                high = mid-1;
+            }
+            }
+             ans.push_back(fr);
+        
+             low=0;
+             high=n-1;
+             fr =-1;
+     
+        
+            while(low<=high){
+            int mid = low + (high-low)/2;
+            if(nums[mid]==target){
+            fr = mid;
+            low = mid+1;
+            }
+            else if(nums[mid]<target){
+                low = mid +1;
+            }
+            else{
+                high = mid-1;
+            }
+            }
+            ans.push_back(fr);
+        
         return ans;
-    }
+    };
 };
+
+
+
+
+
+
+
+
+
+
+//BRUTE-FORCE
+// class Solution {
+// public:
+//     vector<int> searchRange(vector<int>& nums, int target) {
+//         int n = nums.size();
+//         vector <int> ans;
+//         int start = -1;
+//         int end = -1;
+        
+//         for(int i =0; i< n ;i++){
+//             if(nums[i]==target){
+//                 start =i;
+//                 break;
+//             }    
+//         }
+        
+//           for(int i =n-1; i>= 0 ;i--){
+//             if(nums[i]==target){
+//                 end =i;
+//                 break;
+//             }    
+//         }
+//         ans.push_back(start);
+//         ans.push_back(end);
+//         return ans;
+//     }
+// };
