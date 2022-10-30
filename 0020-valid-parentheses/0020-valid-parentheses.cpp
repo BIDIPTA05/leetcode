872e0de44f13fc6cd8bba25c2da2c275acb7470d
Bidipta Saikia <76623158+BIDIPTA05@users.noreply.github.com>
2022-10-30 20:49:39 +0530

    Time: 4 ms (34.14%), Space: 6.3 MB (79.96%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack <char> st;
+        for(auto c : s){
+            if(st.empty()){
+                st.push(c);
+            }
+            else if((st.top()=='(' && c==')') ||  
+                    (st.top()=='{' && c=='}') || 
+                    (st.top()=='[' && c==']'))
+            {
+                st.pop();
+            }
+            else{
+                st.push(c);
+            }
+            
+        }
+     if(st.empty())
+         return true;
+        return false;
+     
+    };
+};
