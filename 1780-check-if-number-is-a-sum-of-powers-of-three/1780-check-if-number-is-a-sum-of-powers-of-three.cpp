6bac1eda94ee4877640e832ca7b95708d2845e52
Bidipta Saikia <76623158+BIDIPTA05@users.noreply.github.com>
2023-04-21 01:30:48 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (97.33%) - LeetHub

---
+++ 1780-check-if-number-is-a-sum-of-powers-of-three/1780-check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool checkPowersOfThree(int n) {
+        while(n>0){
+            if(n%3!=0 && n%3!=1 ){
+                return false;
+            }
+            n =n/3;
+        }
+        return true;
+    }
+};
